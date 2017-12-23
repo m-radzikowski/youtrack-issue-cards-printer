e44f439a2099b13332de0fc44ac7f1f6ba344d9f
Maciej Radzikowski <mradzikowski@blastlab.co>
2017-12-23 16:49:28 +0100

    Wrapped JS scripts in function calls

--- background/page-action.js
+++ background/page-action.js
@@ -1,72 +1,74 @@
-function getLocation(href) {
-	const l = document.createElement("a");
-	l.href = href;
-	return l;
-}
-
-function getQueryVariable(location, variable, def) {
-	const query = location.search.substring(1);
-	const vars = query.split('&');
-	for (let i = 0; i < vars.length; i++) {
-		const pair = vars[i].split('=');
-		if (decodeURIComponent(pair[0]) === variable) {
-			return decodeURIComponent(pair[1]);
-		}
+(function () {
+	function getLocation(href) {
+		const l = document.createElement("a");
+		l.href = href;
+		return l;
 	}
-	return def;
-}
 
-function fetchIssues(url, callback) {
-	const xhr = new XMLHttpRequest();
-
-	xhr.onreadystatechange = function (e) {
-		if (xhr.readyState === 4) {
-			if (xhr.status === 200) {
-				const data = JSON.parse(xhr.responseText);
-				callback(data);
-			} else {
-				console.error('Error fetching issues, http code: ' + xhr.status, e, xhr);
-				// TODO Open error page / documentation
+	function getQueryVariable(location, variable, def) {
+		const query = location.search.substring(1);
+		const vars = query.split('&');
+		for (let i = 0; i < vars.length; i++) {
+			const pair = vars[i].split('=');
+			if (decodeURIComponent(pair[0]) === variable) {
+				return decodeURIComponent(pair[1]);
 			}
 		}
-	};
-
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader('Accept', 'application/json');
-	xhr.send();
-}
-
-function findAndPrintIssues(tab) {
-	chrome.tabs.executeScript(tab.id, {file: 'background/find-issues-per-page.js'}, function (results) {
-		const issuesPerPage = parseInt(results[0]) || 100;
-
-		const location = getLocation(tab.url);
-		const origin = location.origin;
-		const skipIssues = getQueryVariable(location, 'p', 0);
-
-		let query = getQueryVariable(location, 'q', ''); // TODO Search context - currently works for "everything", see: https://youtrack.jetbrains.com/issue/JT-45011
-		if (!query.includes('sort by')) { // add default sort - by updated
-			query += ' sort by: updated';
-		}
-
-		const restUrl = origin + '/rest/issue?' +
-			'filter=' + encodeURIComponent(query) +
-			'&max=' + issuesPerPage +
-			'&after=' + skipIssues;
-
-		fetchIssues(restUrl, function (data) {
-			singleton.getInstance().data = data;
-			chrome.tabs.create({url: chrome.runtime.getURL('print/print.html')});
-		});
-	});
-}
-
-chrome.runtime.onMessage.addListener(function (request, sender) {
-	if (sender.tab && request.action === 'print-youtrack-issues') {
-		findAndPrintIssues(sender.tab);
+		return def;
 	}
-});
 
-chrome.pageAction.onClicked.addListener(function (tab) {
-	findAndPrintIssues(tab);
-});
+	function fetchIssues(url, callback) {
+		const xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function (e) {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					const data = JSON.parse(xhr.responseText);
+					callback(data);
+				} else {
+					console.error('Error fetching issues, http code: ' + xhr.status, e, xhr);
+					// TODO Open error page / documentation
+				}
+			}
+		};
+
+		xhr.open("GET", url, true);
+		xhr.setRequestHeader('Accept', 'application/json');
+		xhr.send();
+	}
+
+	function findAndPrintIssues(tab) {
+		chrome.tabs.executeScript(tab.id, {file: 'background/find-issues-per-page.js'}, function (results) {
+			const issuesPerPage = parseInt(results[0]) || 100;
+
+			const location = getLocation(tab.url);
+			const origin = location.origin;
+			const skipIssues = getQueryVariable(location, 'p', 0);
+
+			let query = getQueryVariable(location, 'q', ''); // TODO Search context - currently works for "everything", see: https://youtrack.jetbrains.com/issue/JT-45011
+			if (!query.includes('sort by')) { // add default sort - by updated
+				query += ' sort by: updated';
+			}
+
+			const restUrl = origin + '/rest/issue?' +
+				'filter=' + encodeURIComponent(query) +
+				'&max=' + issuesPerPage +
+				'&after=' + skipIssues;
+
+			fetchIssues(restUrl, function (data) {
+				singleton.getInstance().data = data;
+				chrome.tabs.create({url: chrome.runtime.getURL('print/print.html')});
+			});
+		});
+	}
+
+	chrome.runtime.onMessage.addListener(function (request, sender) {
+		if (sender.tab && request.action === 'print-youtrack-issues') {
+			findAndPrintIssues(sender.tab);
+		}
+	});
+
+	chrome.pageAction.onClicked.addListener(function (tab) {
+		findAndPrintIssues(tab);
+	});
+})();
--- print/print.js
+++ print/print.js
@@ -1,78 +1,80 @@
-function cloneObject(obj) {
-	return JSON.parse(JSON.stringify(obj))
-}
+(function () {
+	function cloneObject(obj) {
+		return JSON.parse(JSON.stringify(obj))
+	}
 
-function capitalizeFirstLetter(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
-}
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
 
-function lowercaseFirstLetter(string) {
-	return string.charAt(0).toLowerCase() + string.slice(1);
-}
+	function lowercaseFirstLetter(string) {
+		return string.charAt(0).toLowerCase() + string.slice(1);
+	}
 
-function convertIssueFieldArray(issue) {
-	issue.field = issue.field.reduce(function (total, current) {
-		let name = current.name
-			.split(' ')
-			.map(value => capitalizeFirstLetter(value))
-			.join('');
-		name = lowercaseFirstLetter(name);
+	function convertIssueFieldArray(issue) {
+		issue.field = issue.field.reduce(function (total, current) {
+			let name = current.name
+				.split(' ')
+				.map(value => capitalizeFirstLetter(value))
+				.join('');
+			name = lowercaseFirstLetter(name);
 
-		total[name] = current.value;
-		if ('color' in current) {
-			total[name + 'Color'] = current.color;
-		}
-		return total;
-	}, {});
-}
+			total[name] = current.value;
+			if ('color' in current) {
+				total[name + 'Color'] = current.color;
+			}
+			return total;
+		}, {});
+	}
 
-function setCardElementText(card, className, content) {
-	const element = card.getElementsByClassName(className)[0];
+	function setCardElementText(card, className, content) {
+		const element = card.getElementsByClassName(className)[0];
 
-	if (content !== undefined) {
-		let value;
-		if (typeof content === 'string') {
-			value = content;
+		if (content !== undefined) {
+			let value;
+			if (typeof content === 'string') {
+				value = content;
+			} else {
+				value = content[0];
+			}
+
+			element.getElementsByTagName('span')[0].innerHTML = value;
 		} else {
-			value = content[0];
+			element.className += ' hide';
 		}
-
-		element.getElementsByTagName('span')[0].innerHTML = value;
-	} else {
-		element.className += ' hide';
 	}
-}
 
-function setCardElementColor(card, className, color) {
-	const element = card.getElementsByClassName(className)[0].getElementsByTagName('span')[0];
+	function setCardElementColor(card, className, color) {
+		const element = card.getElementsByClassName(className)[0].getElementsByTagName('span')[0];
 
-	if (color !== undefined && color !== null) {
-		element.style.background = color.bg;
-		element.style.color = color.fg;
+		if (color !== undefined && color !== null) {
+			element.style.background = color.bg;
+			element.style.color = color.fg;
+		}
 	}
-}
 
-const data = cloneObject(chrome.extension.getBackgroundPage().singleton.getInstance().data);
+	const data = cloneObject(chrome.extension.getBackgroundPage().singleton.getInstance().data);
 
-const cardTemplate = document.getElementById('card-template');
-const issuesWrapper = document.getElementById('issues-wrapper');
+	const cardTemplate = document.getElementById('card-template');
+	const issuesWrapper = document.getElementById('issues-wrapper');
 
-data.issue.forEach(function (issue) {
-	convertIssueFieldArray(issue);
+	data.issue.forEach(function (issue) {
+		convertIssueFieldArray(issue);
 
-	const card = cardTemplate.cloneNode(true);
-	card.id = '';
+		const card = cardTemplate.cloneNode(true);
+		card.id = '';
 
-	issuesWrapper.appendChild(card);
+		issuesWrapper.appendChild(card);
 
-	setCardElementText(card, 'issue-id', issue.id);
-	setCardElementText(card, 'issue-summary', issue.field.summary);
-	setCardElementText(card, 'issue-type', issue.field.type);
-	setCardElementColor(card, 'issue-type', issue.field.typeColor);
-	setCardElementText(card, 'issue-points', issue.field.storyPoints);
-	setCardElementText(card, 'issue-priority', issue.field.priority);
-	setCardElementColor(card, 'issue-priority', issue.field.priorityColor);
-});
+		setCardElementText(card, 'issue-id', issue.id);
+		setCardElementText(card, 'issue-summary', issue.field.summary);
+		setCardElementText(card, 'issue-type', issue.field.type);
+		setCardElementColor(card, 'issue-type', issue.field.typeColor);
+		setCardElementText(card, 'issue-points', issue.field.storyPoints);
+		setCardElementText(card, 'issue-priority', issue.field.priority);
+		setCardElementColor(card, 'issue-priority', issue.field.priorityColor);
+	});
 
-window.print();
-setTimeout(window.close, 0);
+	window.print();
+	setTimeout(window.close, 0);
+})();
