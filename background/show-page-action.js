4c5f4932d2e947a4c240cf5d9f348a0b4f3a94a3
Maciej Radzikowski <mradzikowski@blastlab.co>
2017-12-23 19:54:00 +0100

    Don't show extension on YT search error

--- background/show-page-action.js
+++ background/show-page-action.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(function () {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [
 				new chrome.declarativeContent.PageStateMatcher({
-					css: ['yt-header', '.issues-center', '.issues-wrapper']
+					css: ['yt-header', '.issues-center', '.issues-wrapper', '.issueContainer']
 				})
 			],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
