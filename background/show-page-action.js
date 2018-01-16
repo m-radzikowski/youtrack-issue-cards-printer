d7f757a0489bfa78b9126ece42e43a3d6b332107
Maciej Radzikowski <maciej@radzikowski.com.pl>
2018-01-16 22:38:31 +0100

    Improved show/hide page action As YouTrack loads and modifies content dynamically, declarativeContent matchers did not work properly in all cases. Instead we search for YT print buttons in page content and activate extension if one is found.

--- background/show-page-action.js
+++ background/show-page-action.js
@@ -1,12 +1,13 @@
-chrome.runtime.onInstalled.addListener(function () {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [
-				new chrome.declarativeContent.PageStateMatcher({
-					css: ['yt-header', '.issues-center', '.issues-wrapper', '.issueContainer']
-				})
-			],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
+function updateVisibility(tabId) {
+	chrome.tabs.sendMessage(tabId, {action: 'find-yt-print-button'}, function (hasPrintButton) {
+		if (hasPrintButton) {
+			chrome.pageAction.show(tabId);
+		}
 	});
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
+	if (change.status === "complete") {
+		updateVisibility(tabId);
+	}
 });
--- content/replace-print-button.js
+++ content/replace-print-button.js
@@ -1,13 +1,33 @@
 (function () {
-	console.log('a');
-	const button = document.getElementById('id_l.I.c.tb.printIssues');
-	console.log('x', button);
+	/**
+	 * On regular issues list page and on issues list for tag or saved search context print button id differs.
+	 */
+	function findPrintButton() {
+		const ids = ['id_l.I.c.tb.printIssues', 'id_l.T.Issues.c.tb.printIssues', 'id_l.S.Issues.c.tb.printIssues'];
+		for (let i = 0; i < ids.length; i++) {
+			const button = document.getElementById(ids[i]);
+			if (button) {
+				return button;
+			}
+		}
+		return null;
+	}
+
+	const button = findPrintButton();
+
 	if (button) {
-		console.log('b');
 		button.removeAttribute('cn'); // attribute that triggers default print page load
 		button.onclick = function (e) {
 			chrome.runtime.sendMessage({action: "print-youtrack-issues"});
 			e.preventDefault();
 		};
+	}
+
+	if (window === top) {
+		chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
+			if (req.action === 'find-yt-print-button') {
+				sendResponse(button !== null);
+			}
+		});
 	}
 })();
