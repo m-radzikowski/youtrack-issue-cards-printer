b8f77844e8a2a4c00aad3e3b6b67171ec68812c0
Maciej Radzikowski <maciej@radzikowski.com.pl>
2018-08-25 23:27:40 +0200

    Issue links parsing and correct subtask of display

--- print/print.js
+++ print/print.js
@@ -3,12 +3,11 @@
 		return JSON.parse(JSON.stringify(obj))
 	}
 
-	function capitalizeFirstLetter(string) {
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	}
-
-	function lowercaseFirstLetter(string) {
-		return string.charAt(0).toLowerCase() + string.slice(1);
+	function camelize(str) { // https://stackoverflow.com/a/2970667/2512304
+		return str.replace('-', ' ').replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+			if (+match === 0) return "";
+			return index === 0 ? match.toLowerCase() : match.toUpperCase();
+		});
 	}
 
 	/**
@@ -17,28 +16,25 @@
 	 * Additionally, there are added keys:
 	 * 1. id with issue ID.
 	 * 2. xxxColor for fields containing "color" parameter, with "bg" and "fg" fields, where xxx is the field's name.
-	 * 3. linkXxx for issue links, containing array of links of given type, where Xxx is the link's type.
+	 * 3. linkXxxYyy for issue links, containing array of links of given type, where Xxx is the link's type and Yyy is the links role.
 	 */
 	function getIssueFields(issue) {
 		const fields = issue.field.reduce(function (fields, field) {
-			let name = field.name
-				.split(' ')
-				.map(value => capitalizeFirstLetter(value))
-				.join('');
-			name = lowercaseFirstLetter(name);
-
-			fields[name] = field.value;
-
-			if ('color' in field) {
-				fields[name + 'Color'] = field.color;
-			}
+			const name = camelize(field.name);
 
 			if (name === 'links') {
 				const links = field.value.reduce(function (types, link) {
-					(types['link' + link['type']] = types['link' + link['type']] || []).push(link.value);
+					const linkName = camelize(`link ${link['type']} ${link['role']}`);
+					(types[linkName] = types[linkName] || []).push(link.value);
 					return types;
 				}, {});
 				Object.assign(fields, links);
+			} else {
+				fields[name] = field.value;
+
+				if ('color' in field) {
+					fields[name + 'Color'] = field.color;
+				}
 			}
 			return fields;
 		}, {});
@@ -74,7 +70,7 @@
 		// despite font is loaded in CSS before the JS, sometimes print page opens with default font
 		// opening print window with little delay should solve this problem, as browser should display font
 		// properly up to this point
-		setTimeout(window.print, 100);
+		setTimeout(window.print, 200);
 
 		// close page after exit from print mode
 		setTimeout(window.close, 200);
