b4e1dde03707ffa59b7e050dc64a13a8b84c9cb2
Maciej Radzikowski <maciej@radzikowski.com.pl>
2018-08-26 11:04:56 +0200

    Changed CSS class names to more generic

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,9 +1,9 @@
 const DEFAULT_TEMPLATE = `<div class="magnet-place"></div>
 <div class="issue-summary" data-field="summary"><span></span></div>
 <div class="issue-id" data-field="id"><span></span></div>
-<div class="issue-parent-id" data-field="linkSubtaskSubtaskOf">subtask of <span></span></div>
-<div class="issue-type" data-field="type"><span></span></div>
-<div class="issue-subsystem" data-field="subsystem"><span></span></div>
+<div class="issue-id-caption" data-field="linkSubtaskSubtaskOf">subtask of <span></span></div>
+<div class="issue-top-label issue-right" data-field="type"><span></span></div>
+<div class="issue-top-label issue-left" data-field="subsystem"><span></span></div>
 <div class="issue-points" data-field="storyPoints"><span></span></div>
 <div class="issue-priority" data-field="priority"><span></span></div>`;
 
